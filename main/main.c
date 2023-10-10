d626025d8bcea5e75d750d6806d80569a051e297
Luis <luis.borsoi@sap.com>
2023-10-10 18:06:18 -0300

    update

--- main/main.c
+++ main/main.c
@@ -15,7 +15,6 @@ static const int RX_BUF_SIZE = 1024;
 #define TXD_PIN (GPIO_NUM_17)
 #define RXD_PIN (GPIO_NUM_16)
 
-
 #define PINO_LED 22
 #define PIN_LED_UMIDADE 23
 #define PIN_LED_TEMPERATURA 21
@@ -23,7 +22,6 @@ static const int RX_BUF_SIZE = 1024;
 #define DEFAULT_VREF 1100 // Use adc2_vref_to_gpio() to obtain a better estimate
 #define NO_OF_SAMPLES 50  // Multisampling
 
- 
 // Configuração do ADC
 static const adc_channel_t channel = ADC_CHANNEL_6; // GPIO34 CHANNEL_6
 static const adc_bits_width_t width = ADC_WIDTH_BIT_12;
@@ -32,15 +30,17 @@ static const adc_atten_t atten = ADC_ATTEN_DB_11; // ver https://docs.espressif.
 // Configuração do LED
 bool estado_led = 0;
 int adc_reading = 0;
-float umidade =0.0;
-float temperatura=0.0;
+float umidade = 0.0;
+float temperatura = 0.0;
 char *test_str = "\n";
 char umidade_str[20];
 char temperatura_str[20];
- 
-//Configurações iniciais da porta UART 
 
-void init(void) {
+int nRetorno = 0;
+// Configurações iniciais da porta UART
+
+void init(void)
+{
     const uart_config_t uart_config = {
         .baud_rate = 115200,
         .data_bits = UART_DATA_8_BITS,
@@ -55,7 +55,7 @@ void init(void) {
     uart_set_pin(UART_NUM_2, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
 }
 
-int sendData(const char* logName, const char* data)
+int sendData(const char *logName, const char *data)
 {
     const int len = strlen(data);
     const int txBytes = uart_write_bytes(UART_NUM_2, data, len);
@@ -64,15 +64,66 @@ int sendData(const char* logName, const char* data)
 static void tx_task(void *arg)
 {
     static const char *TX_TASK_TAG = "TX_TASK";
-    while (1) {
-                   
+    while (1)
+    {
+
+        if (nRetorno == 1)
+        {
             // Envie as strings pela UART
             sendData(TX_TASK_TAG, umidade_str);
             sendData(TX_TASK_TAG, temperatura_str);
+            nRetorno = 0;
+        }
+
+        if (nRetorno == 2)
+        {
+            // Envie as strings pela UART
+            sendData(TX_TASK_TAG, umidade_str);
+            nRetorno = 0;
+        }
+
+        if (nRetorno == 3)
+        {
+            // Envie as strings pela UART
+            sendData(TX_TASK_TAG, temperatura_str);
+            nRetorno = 0;
+        }
 
         vTaskDelay(10000 / portTICK_PERIOD_MS);
     }
 }
+
+void rx_task(void *arg)
+{
+    static const char *RX_TASK_TAG = "RX_TASK";
+    uint8_t *data = (uint8_t *)malloc(RX_BUF_SIZE + 1);
+    while (1)
+    {
+        const int rxBytes = uart_read_bytes(UART_NUM_2, data, RX_BUF_SIZE, 1000 / portTICK_PERIOD_MS);
+        if (rxBytes > 0)
+        {
+            if (rxBytes == 1 && data[0] == '1')
+            {
+                // A sequência em data é igual a "1"
+                nRetorno = 1; // ou outra ação que você deseja executar
+            }
+
+            if (rxBytes == 1 && data[0] == '2')
+            {
+                // A sequência em data é igual a "1"
+                nRetorno = 2; // ou outra ação que você deseja executar
+            }
+
+            if (rxBytes == 1 && data[0] == '3')
+            {
+                // A sequência em data é igual a "1"
+                nRetorno = 3; // ou outra ação que você deseja executar
+            }
+        }
+    }
+    free(data);
+}
+
 void tarefaADC(void *parametros)
 {
     // Configure ADC
@@ -80,130 +131,126 @@ void tarefaADC(void *parametros)
     adc1_config_channel_atten(channel, atten);
     for (;;)
     {
-       
+
         // Multisampling
         for (int i = 0; i < NO_OF_SAMPLES; i++)
         {
             adc_reading += adc1_get_raw((adc1_channel_t)channel);
         }
         adc_reading /= NO_OF_SAMPLES;
-        //printf("ADC: %d\n", adc_reading);
+        // printf("ADC: %d\n", adc_reading);
 
-        // caso o LDR identifique que está escuro, ele aciona o LED 
-        if (adc_reading > 3000){
-             estado_led =1;
-        }
-        else 
+        // caso o LDR identifique que está escuro, ele aciona o LED
+        if (adc_reading > 3000)
         {
-             estado_led =0;
+            estado_led = 1;
         }
-    
+        else
+        {
+            estado_led = 0;
+        }
+
         vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
 
 void tarefaLED(void *parametros)
 {
- 
+
     for (;;)
     {
         gpio_set_level(PINO_LED, estado_led);
         vTaskDelay(10 / portTICK_PERIOD_MS);
 
-        //Caso a umidade esteja muito baixa, sinalizo nível crítico com LED VERMELHO
+        // Caso a umidade esteja muito baixa, sinalizo nível crítico com LED VERMELHO
         if (umidade < 50.00)
         {
             gpio_set_level(PIN_LED_UMIDADE, 1);
             vTaskDelay(10 / portTICK_PERIOD_MS);
-        }else 
+        }
+        else
         {
             gpio_set_level(PIN_LED_UMIDADE, 0);
             vTaskDelay(10 / portTICK_PERIOD_MS);
         }
 
-           //Caso a temperatura esteja muito alta, sinalizo com um LED AZUL para simular o acionamento de um dispositivo para reduzir a temperatura. 
+        // Caso a temperatura esteja muito alta, sinalizo com um LED AZUL para simular o acionamento de um dispositivo para reduzir a temperatura.
         if (temperatura > 25.00)
         {
             gpio_set_level(PIN_LED_TEMPERATURA, 1);
             vTaskDelay(10 / portTICK_PERIOD_MS);
-        }else {
+        }
+        else
+        {
             gpio_set_level(PIN_LED_TEMPERATURA, 0);
             vTaskDelay(10 / portTICK_PERIOD_MS);
         }
-    
-
     }
 }
 
 void tarefaDHT(void *parametros)
 {
-    //Realizar a leitura do sensor de temperatura e humidade. 
+    // Realizar a leitura do sensor de temperatura e humidade.
 
+    setDHTgpio(GPIO_NUM_27);
 
-	setDHTgpio(GPIO_NUM_27);
+    while (1)
+    {
 
-	while(1) {
-	
-		//printf("DHT Sensor Readings\n" );
-		int ret = readDHT();
-		
-        if (ret == DHT_OK){
-             umidade = getHumidity();
-             temperatura = getTemperature();
+        // printf("DHT Sensor Readings\n" );
+        int ret = readDHT();
+
+        if (ret == DHT_OK)
+        {
+            umidade = getHumidity();
+            temperatura = getTemperature();
 
             // Formate os valores de umidade e temperatura como strings
- 
+
             snprintf(umidade_str, sizeof(umidade_str), "Umidade: %.2f %%\n", umidade);
             snprintf(temperatura_str, sizeof(temperatura_str), "Temperatura: %.2f", temperatura);
-
-
-
         }
-        else 
-		    errorHandler(ret);
+        else
+            errorHandler(ret);
 
         /*printf("Humidity %.2f %%\n", getHumidity());
-		printf("Temperature %.2f degC\n\n", getTemperature());*/
-		
-		vTaskDelay(3000 /portTICK_PERIOD_MS);
-	}
-    
-}
+        printf("Temperature %.2f degC\n\n", getTemperature());*/
 
+        vTaskDelay(3000 / portTICK_PERIOD_MS);
+    }
+}
 
 void app_main(void)
 {
     gpio_reset_pin(PINO_LED);
-    gpio_set_direction(PINO_LED,GPIO_MODE_OUTPUT);
-    
+    gpio_set_direction(PINO_LED, GPIO_MODE_OUTPUT);
+
     gpio_reset_pin(PIN_LED_UMIDADE);
-    gpio_set_direction(PIN_LED_UMIDADE,GPIO_MODE_OUTPUT);
+    gpio_set_direction(PIN_LED_UMIDADE, GPIO_MODE_OUTPUT);
     gpio_reset_pin(PIN_LED_TEMPERATURA);
-    gpio_set_direction(PIN_LED_TEMPERATURA,GPIO_MODE_OUTPUT);
+    gpio_set_direction(PIN_LED_TEMPERATURA, GPIO_MODE_OUTPUT);
     bool estado_anterior = estado_led;
 
-
-    //Configurações da UART 
+    // Configurações da UART
     init();
-    
+
     xTaskCreate(tarefaADC, "adc", 2048, NULL, 0, NULL);
     xTaskCreate(tarefaLED, "led", 2048, NULL, 0, NULL);
     xTaskCreate(tarefaDHT, "dht", 2048, NULL, 0, NULL);
-    xTaskCreate(tx_task, "uart_tx_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
+    xTaskCreate(tx_task, "uart_tx_task", 1024 * 2, NULL, configMAX_PRIORITIES - 1, NULL);
+    xTaskCreate(rx_task, "uart_rx_task", 1024 * 2, NULL, configMAX_PRIORITIES, NULL);
 
-
-        for (;;)
+    for (;;)
     {
-        
-        if (estado_led != estado_anterior){
+
+        if (estado_led != estado_anterior)
+        {
             estado_anterior = estado_led;
             if (estado_led == 0)
                 printf("ADC: %d  LED DESLIGADO\n", adc_reading);
-            else 
+            else
                 printf("ADC: %d  LED LIGADO \n", adc_reading);
         }
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-
     }
 }
-
